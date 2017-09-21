79a99e9d4bf477f0dac6fbb9a108e3ede660881c
karthik-kashyap <kashyapkarthik3@gmail.com>
2017-09-21 17:46:37 +0530

    taken input

--- wp.py
+++ wp.py
@@ -1,3 +1,7 @@
 from itertools import permutations
-print(set(permutations([1,1,1,3])))
-print(set(permutations("abcdbac",4)))
+#print(set(permutations([1,1,1,3])))
+#print(set(permutations("abcdbac",4)))
+ow=print("Enter ow:")
+tw=print("Enter tw:")
+dist=len(tw)-len(ow)+1
+
